ab48ae861e019a326ebbfc698bd548ba4d9e5f6a
valfaro123 <valfaro6@horizon.csueastbay.edu>
2021-12-23 19:46:09 -0800

    add tailwind config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'body': '#121619',
+        'selected-text': '#0000ff',
+        'theme': '#ff764d',
+        'color-block-1': '#fbffa7',
+        'color-block-2': '#b6ffc0',
+        'color-block-3': '#d5b3ff',
+        'color-block-4': '#b1c5ff',
+        'input': '#2A2A35',
+      },
+      fontFamily: {
+        
+      }
+    },
   },
   plugins: [],
 }
