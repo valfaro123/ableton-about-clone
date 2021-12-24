f91925dead43dae980e277016374010f019be544
valfaro123 <valfaro6@horizon.csueastbay.edu>
2021-12-23 19:48:47 -0800

    add nav, text sections and header components to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 
 import './App.css';
+import Header from './components/header/Header';
+import ImageCard1 from './components/image-cards/ImageCard1';
+import Navbar from './components/navbar/Navbar';
+import TextSection from './components/text-section/TextSection';
+import {textContent} from './components/text-section/textSectionContent'
 
 function App() {
+  const {p1,p2,p3,p4,p5,p6} = textContent;
+  const test = () => {
+    console.log("");
+    
+  }
   return (
-    <div className="App">
-      test
+    <div>
+      <Navbar/>
+      <Header/>
+      <TextSection key={p1.key} title={p1.title} body={p1.body}/>
+      <ImageCard1/>
+      <TextSection key={p2.key} title={p2.title} body={p2.body}/>
+      <TextSection key={p3.key} title={p3.title} body={p3.body}/>
+      <TextSection key={p4.key} title={p4.title} body={p4.body}/>
+      <TextSection key={p5.key} title={p5.title} body={p5.body}/>
+      <TextSection key={p6.key} title={p6.title} body={p6.body}/>
+      
     </div>
   );
 }
 
 export default App;
+// p={textContentBody.p1}
