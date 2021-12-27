0f302444c07c6ff7e968b8f6b0ca776d94b1d3b1
valfaro123 <valfaro6@horizon.csueastbay.edu>
2021-12-26 20:01:55 -0800

    add imagecard3 component

---
+++ src/components/image-cards/ImageCard3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import posterImg from '../../images/poster-meet-the-makers.jpg'
+export default function ImageCard3() {
+    return (
+        <div className='mx-8 md:mx-24 my-12 '>
+            <img src={posterImg}/>
+        </div>
+    )
+}
