b3781e12669a4059e7191c97c7b04c16399e2229
valfaro123 <valfaro6@horizon.csueastbay.edu>
2021-12-29 19:43:40 -0800

    add image card components  to app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import ImageCard1 from './components/image-cards/ImageCard1';
+import ImageCard2 from './components/image-cards/ImageCard2';
 import ImageCard3 from './components/image-cards/ImageCard3';
+import ImageCard4 from './components/image-cards/ImageCard4';
 import ImageCard5 from './components/image-cards/ImageCard5';
 import Navbar from './components/navbar/Navbar';
 import TextSection from './components/text-section/TextSection';
@@ -18,15 +20,15 @@ function App() {
       <Navbar/>
       <Header/>
       <TextSection key={p1.key} title={p1.title} body={p1.body}/>
-      {/* <ImageCard1/> */}
+      <ImageCard1/>
       <TextSection key={p2.key} title={p2.title} body={p2.body}/>
       <VideoSection/>
       <TextSection key={p3.key} title={p3.title} body={p3.body}/>
-      {/* <ImageCard2/> */}
+      <ImageCard2/>
       <TextSection key={p4.key} title={p4.title} body={p4.body}/>
       <ImageCard3/>
       <TextSection key={p5.key} title={p5.title} body={p5.body}/>
-      {/* ImageCard4 */}
+      <ImageCard4/>
       <TextSection key={p6.key} title={p6.title} body={p6.body}/>
       <ImageCard5/>
       <Footer/>
